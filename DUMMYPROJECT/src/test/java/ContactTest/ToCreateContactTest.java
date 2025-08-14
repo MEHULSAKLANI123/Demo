6e8cb82de4c1d051bee4c35a14bd9e7ba42df9bd
Mehul <mehulsaklanics.18@gmail.com>
2025-08-14 09:15:07 +0530

    FIRST PUSH

---
+++ DUMMYPROJECT/src/test/java/ContactTest/ToCreateContactTest.java
@@ -0,0 +1,9 @@
+package ContactTest;
+
+public class ToCreateContactTest {
+
+	public static void main(String[] args) {
+		System.out.println("Contact Created Successfully");
+	}
+
+}
---
+++ DUMMYPROJECT/src/test/java/OrganisationTest/ToCreateOrganisationTest.java
@@ -0,0 +1,9 @@
+package OrganisationTest;
+
+public class ToCreateOrganisationTest {
+
+	public static void main(String[] args) {
+		System.out.println("Organisation Created Successfully");
+	}
+
+}
